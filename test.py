e51178c79b93bb5c93cb3e8fb0e1d2c313367ed0
chaosmak <chaosmak789@gmail.com>
2017-04-28 12:59:22 +0500

    ver 2

--- test.py
+++ test.py
@@ -15,12 +15,25 @@ def encode(image_name, input_name, output_name, n):
     byte_file = get_bytes_from_file(input_name)
     bin_msg = bytes_to_bits(byte_file)
     pos = 0
-    if len(bin_msg) <= n*3*width*height-1:
+    msg_len = len(bin_msg)
+    if msg_len <= n*3*width*height-5:
         new_image = Image.new('RGB', (width, height))
         # поставим в пиксель (0,0) информацию, что есть сообщение
+        r, g, b = image.getpixel((0, 0))
+        # пусть будем распознавать есть ли инфа по "111" в последних трех битах
+        r = edit_last_n(r, [1, 1, 1], 3)
+        g = edit_last_n(g, [1, 1, 1], 3)
+        b = edit_last_n(b, [1, 1, 1], 3)
+        new_image.putpixel((0, 0), (r, g, b))
+        # Отводим 4 пикселя:(1 - 5, 0) на служебную инфу, т.е. в каком пикселе будет заканчиваться текст
+        # В первом - сколько раз взять по 255
+        # Во втором - сколько прибавить после этих взятий по 255
+        # Так два раза, чтоб когда будем расшифровывать - узнать до какого надо расшифровывать
+        service_i = service_j = 0
         for i in range(width):
             for j in range(height):
-                #if i != 0 and j != 0:
+                if i < 5 and j == 0:
+                    continue
                 r, g, b = image.getpixel((i, j))
                 try:
                     bin_msg_array = bin_msg[pos:pos+n*3]
@@ -31,7 +44,10 @@ def encode(image_name, input_name, output_name, n):
                     g = edit_last_n(g, bin_msg_array[n:2*n], n)
                     b = edit_last_n(b, bin_msg_array[2*n:3*n], n)
                     pos += 3*n
+                    service_i = i
+                    service_j = j
                 new_image.putpixel((i, j), (r, g, b))
+        new_image = add_info(image, new_image, service_i, service_j)
         new_image.save(output_name)
         new_image.close()
     else:
@@ -39,32 +55,49 @@ def encode(image_name, input_name, output_name, n):
     image.close()
 
 
-def edit_last_n(color, paste, n=1):
-    b = "{0:08b}".format(color)
-    i = 0
-    for num in paste:
-        #bit = int(paste[i])
-        if num == 0:
-            b = b[:8-n + i] + "0" + b[8-n +i+ 1:]
-        else:
-            b = b[:8-n + i] + "1" + b[8-n +i+ 1:]
-        i += 1
-    print("b", b)
-    b = int(b, 2)
-    print(b)
-    return b
+def add_info(old_image, new_image, i, j):
+    temp = ["{0:08b}".format(i // 255),
+            "{0:08b}".format(i - ((i // 255) * 255)),
+            "{0:08b}".format(j // 255),
+            "{0:08b}".format(j - ((j // 255) * 255))]
+    for pos in range(1, 5):
+        pixels = old_image.getpixel((pos, 0))
+        modified_pixels = edit_c(pixels, temp[pos-1])
+        new_image.putpixel((pos, 0), modified_pixels)
+    return new_image
+
+
+def edit_c(pixel, info):
+    r, g, b = pixel
+    r = edit_last_n(r, info[:2], 2)
+    g = edit_last_n(g, info[2:5], 3)
+    b = edit_last_n(b, info[5:], 3)
+    return r, g, b
+
+
+def edit_last_n(color, msg, n):
+    """color - int, msg - list, n - int"""
+    msg_len = len(msg)
+    # Если длина сообщения 0, то возвращаем неизмененный цвет
+    if msg_len == 0:
+        return color
+    bits = "{0:08b}".format(color)
+    msg = ''.join(str(i) for i in msg)
+    # Если кол-во бит, которые нужо зашифровать
+    # меньше, чем длина по умолчанию, то пихаем их так, чтоб потом расшифровывать нормально
+    if len(msg) != n:
+        return int(bits[:8-n] + msg + bits[8-n+len(msg):], 2)
+    return int(bits[:8-len(msg)] + msg, 2)
 
 
 def get_bytes_from_file(filename, size=2048):
     result = []
     with open(filename, "rb") as f:
-        while True:
+        chunk = f.read(size)
+        while chunk:
+            for b in chunk:
+                result.append(b)
             chunk = f.read(size)
-            if chunk:
-                for b in chunk:
-                    result.append(b)
-            else:
-                break
     return result
 
 
@@ -90,16 +123,40 @@ def get_last_n_bits_from_color(color, n):
     return bits[-n:]
 
 
+def get_stop_pixels(image):
+    res = []
+    for pos in range(1, 5):
+        temp = ''
+        r, g, b = image.getpixel((pos, 0))
+        temp += get_last_n_bits_from_color(r, 2)
+        temp += get_last_n_bits_from_color(g, 3)
+        temp += get_last_n_bits_from_color(b, 3)
+        res.append(int(temp, 2))
+    return res[0]*255 + res[1], res[2]*255 + res[3]
+
+
 def decode_bmp(image_name, n):
     image = Image.open(image_name)
     width = image.width
     height = image.height
     res = ""
+    stop_i, stop_j = get_stop_pixels(image)
+    for color in image.getpixel((0, 0)):
+        temp = get_last_n_bits_from_color(color, 3)
+        if temp != "111":
+            print("Скорее всего в изображении нет нашего сообщения")
+    stop = False
     for i in range(width):
+        if stop:
+            break
         for j in range(height):
-            for color in image.getpixel((i, j)):
-                # Добавляем последние n бит из цветов картинки в результат
-                res += get_last_n_bits_from_color(color, n)
+            if i == stop_i and j == stop_j + 1:
+                stop = True
+                break
+            if i >= 5 or j != 0:
+                for color in image.getpixel((i, j)):
+                    # Добавляем последние n бит из цветов картинки в результат
+                    res += get_last_n_bits_from_color(color, n)
     image.close()
     result = bits_to_bytes(res)
     f = open('output.txt', 'wb')
@@ -109,7 +166,8 @@ def decode_bmp(image_name, n):
 
 
 def main():
-    encode("test.bmp", "message.txt", "output.bmp", 8)
-    decode_bmp("output.bmp", 8)
+    n = 3
+    encode("test.bmp", "message.txt", "output.bmp", n)
+    decode_bmp("output.bmp", n)
 if __name__ == '__main__':
     main()
